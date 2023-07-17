e3ba62df8054bf7e7f6455620496314faab4fe51
gurp13579 <gugipeled@gmail.com>
2023-07-17 09:21:31 +0300

    Create pythonreviewlab.py

---
+++ pythonreviewlab.py
@@ -0,0 +1,13 @@
+def create_youtube_video(title,description):
+	video = {"title": title, "description": description, "like": 0, "dislikes": 0, "comments":{}}
+def likes(video):
+	if like in video:
+		video["like"] += 1
+		return video
+def dislike(video):
+	if dislikes in video
+	video["dislikes"] += 1
+	return video
+def comments(username,video,comment_txt):
+	video['comments'][username] = comment_txt
+	return video 
